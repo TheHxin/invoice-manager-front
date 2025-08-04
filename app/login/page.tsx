62120e376ecd3ce4f8c8fcb7d11dfe8315599d67
deb <hxin.radin@gmail.com>
2025-08-04 13:21:03 +0200

    Added fetching invoice logic

---
+++ app/login/page.tsx
@@ -0,0 +1,54 @@
+"use client"
+
+// in `app/page.tsx` or `src/app/page.tsx` (if using App Router)
+
+import axios from "axios";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button"; //@ is an alias for the project root path
+// { Button } is when you have multiple exports from one file so you do a "named export"
+// to be able to access a function outside the js, ts or tsx file you need to "export" it -> like public in java or C#
+
+import { useState } from "react";
+
+export default function Login() {
+
+  const [username , setUsername] = useState("");
+  const [password , setPassword] = useState("");
+
+  const auth = (username: string, password: string) => {
+    axios
+      .post("http://localhost:38532/token_json", {
+        username: username,
+        password: password,
+      })
+      .then((res) => {
+        console.log(res.data.access_token);
+
+        localStorage.setItem("token", res.data.access_token);
+      })
+      .catch((err) => {
+        console.log("got fucking error omg", err);
+      });
+  };
+
+  return ( //TODO: make the login page in a form for password manager convinience
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="w-full max-w-sm bg-white p-8 rounded-2xl shadow-xl space-y-6">
+        <h1 className="text-2xl font-semibold text-center">Login</h1>
+        <div className="space-y-4">
+          <Input type="text" placeholder="Username" className="w-full" onChange={(e) => {
+            setUsername(e.target.value);
+          }}/>
+          <Input type="password" placeholder="Password" className="w-full" onChange={(e) => {
+            setPassword(e.target.value)
+          }}/>
+        </div>
+        <Button className="w-full" onClick={() => {
+          auth(username, password);
+        }}>
+          Login
+        </Button>
+      </div>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,85 @@
-"user client"
-// in `app/page.tsx` or `src/app/page.tsx` (if using App Router)
-import { Button } from "@/components/ui/button" //@ is an alias for the project root path
-// { Button } is when you have multiple exports from one file so you do a "named export" 
-// to be able to access a function outside the js, ts or tsx file you need to "export" it -> like public in java or C#
-import { Input } from "@/components/ui/input"
-import {useState} from "react"
-import axios from "axios"
-import { useRouter } from "next/router"
+"use client";
+
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import axios from "axios";
+import { useState } from "react";
+import { useEffect } from "react";
+
+interface Invoice {
+  id: number;
+  origin: string;
+  destination: string;
+  issued: string;
+  due: string;
+  amount: number;
+}
 
 export default function Home() {
+  const [invoices, setInvoices] = useState<Invoice[]>([]);
+
+  const fetchInvoices = async () => {
+    axios
+      .get("http://localhost:38532/invoices", {
+        headers: {
+          'Authorization': "Bearer " + localStorage.getItem('token')
+        },
+      })
+      .then((res) => {
+        setInvoices(res.data);
+      })
+      .catch((e) => {
+        console.log("fuck bruv we got errs: ");
+        console.log(e);
+      });
+  };
+
+  useEffect(() => {
+    const intervalid = setInterval(() => {
+      fetchInvoices();
+    }, 5000);
+
+    return () => clearInterval(intervalid);
+  });
+
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="w-full max-w-sm bg-white p-8 rounded-2xl shadow-xl space-y-6">
-        <h1 className="text-2xl font-semibold text-center">Login</h1>
-        <div className="space-y-4">
-          <Input type="text" placeholder="Username" className="w-full" />
-          <Input type="password" placeholder="Password" className="w-full" />
-        </div>
-        <Button className="w-full">Login</Button>
+    <div className="w-full max-w-5xl mx-auto p-4">
+      <div className="overflow-x-auto rounded-md border">
+        <Table>
+          <TableCaption>Invoices</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[100px]">InvoiceID</TableHead>
+              <TableHead className="text-center">Origin</TableHead>
+              <TableHead className="text-center">Destination</TableHead>
+              <TableHead className="text-center">IssuedDate</TableHead>
+              <TableHead className="text-center">DueDate</TableHead>
+              <TableHead className="text-center">Amount</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {invoices.map((invoice) => (
+              <TableRow key={invoice.id}>
+                <TableCell className="text-center">{invoice.id}</TableCell>
+                <TableCell className="text-center">{invoice.origin}</TableCell>
+                <TableCell className="text-center">
+                  {invoice.destination}
+                </TableCell>
+                <TableCell className="text-center">{invoice.issued}</TableCell>
+                <TableCell className="text-center">{invoice.due}</TableCell>
+                <TableCell className="text-center">${invoice.amount}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
       </div>
     </div>
-  )
+  );
 }
